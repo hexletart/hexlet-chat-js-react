ea272428574a987ae638f4354db7c4d601c5634d
hexlet_art <hexlet_art@mail.ru>
2023-05-28 00:15:52 +0300

    deleted

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDom from 'react-dom/client';
-import init from './init.jsx';
-
-const app = async () => {
-  const root = ReactDom.createRoot(document.getElementById('root'));
-  root.render(await init());
-};
-
-app();
