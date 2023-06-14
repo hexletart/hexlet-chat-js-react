35a8873de1bf92652a0d5dd93922af36c3961cca
hexlet_art <hexlet_art@mail.ru>
2023-06-15 01:57:56 +0300

    rewrited

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, useLocation } from 'react-router-dom';
-import ChatPage from './ChatPage';
+import ChatPage from './pages/ChatPage';
 
 const getAuthHeader = () => {
   const token = JSON.parse(localStorage.getItem('userId'));
@@ -10,7 +10,7 @@ const PrivateRoute = () => {
   const location = useLocation();
   const tokenJSON = getAuthHeader();
   return (
-    tokenJSON ? <ChatPage token={tokenJSON} /> : <Navigate to="/login" state={{ from: location }} />
+    tokenJSON ? <ChatPage tokenJSON={tokenJSON} /> : <Navigate to="/login" state={{ from: location }} />
   );
 };
 
