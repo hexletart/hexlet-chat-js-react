e87131c06516dea7b83832a5739783aa69305fe5
hexlet_art <hexlet_art@mail.ru>
2023-07-16 21:14:10 +0300

    added signingUpPath instead of previous one

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,5 +3,5 @@ const apiPath = '/api/v1/';
 export default {
   loginPath: [apiPath, 'login'].join('/'),
   usersPath: [apiPath, 'data'].join('/'),
-  regPath: [apiPath, 'signup'].join('/'),
+  signingUpPath: [apiPath, 'signup'].join('/'),
 };
