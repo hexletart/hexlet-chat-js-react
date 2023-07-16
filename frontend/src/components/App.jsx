815b841efc8a1aaca6f22451a653deb9986739ea
hexlet_art <hexlet_art@mail.ru>
2023-07-16 21:05:53 +0300

    changed paths in each route and changed AuthProvider

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthContext from '../contexts/index';
+import paths from '../paths.js';
+import AuthContext from '../contexts/AuthContext';
 import Frame from './Frame';
-import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/AuthorizationPage';
 import NotFoundPage from './pages/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
 
 const AuthProvider = ({ children }) => {
-  const logIn = (token) => {
+  const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('userId'));
+  const login = (token, userName) => {
     localStorage.setItem('userId', token);
+    localStorage.setItem('userName', userName);
+    setLoggedIn(true);
   };
-  const logOut = () => {
+  const logout = () => {
     localStorage.removeItem('userId');
+    localStorage.removeItem('userName');
+    setLoggedIn(false);
   };
   return (
-    <AuthContext.Provider value={{ logIn, logOut }}>
+    <AuthContext.Provider value={{ login, logout, loggedIn }}>
       {children}
     </AuthContext.Provider>
   );
@@ -26,9 +33,10 @@ const App = () => (
     <Frame>
       <Router>
         <Routes>
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/" element={<PrivateRoute />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path={paths.any} element={<NotFoundPage />} />
+          <Route path={paths.main} element={<PrivateRoute />} />
+          <Route path={paths.login} element={<LoginPage />} />
+          <Route path={paths.signingUp} element={<RegistrationPage />} />
         </Routes>
       </Router>
     </Frame>
