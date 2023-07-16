bc2e75e55932be9007d3734ed1f60cfce23064ed
hexlet_art <hexlet_art@mail.ru>
2023-07-16 21:12:36 +0300

    imported and used authReducer, socket and middleware

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './channelsSlice.js';
 import messagesReducer from './messagesSlice.js';
+import authedReducer from './authedSlice.js';
+import SocketClient from '../api/SocketClient.js';
+import socketMiddleware from '../middleware/socketMiddleware';
+
+const socket = new SocketClient();
 
 export default configureStore({
   reducer: {
+    authed: authedReducer,
     channels: channelsReducer,
     messages: messagesReducer,
   },
+  middleware: [socketMiddleware(socket)],
 });
