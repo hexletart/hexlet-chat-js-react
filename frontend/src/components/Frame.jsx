ff55f5d0b983241567ed819a12c9fce552332467
hexlet_art <hexlet_art@mail.ru>
2023-07-16 21:07:10 +0300

    added and organised exit button

--- frontend/src/components/Frame.jsx
+++ frontend/src/components/Frame.jsx
@@ -1,5 +1,26 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Button } from 'react-bootstrap';
+import { actions as authedActions } from '../slices/authedSlice';
+import useAuthHook from '../hooks/authHook';
+import paths from '../paths';
+
+const ExitButton = () => {
+  const dispatch = useDispatch();
+  const auth = useAuthHook();
+  const handleLogout = () => {
+    console.log('from logout by frame');
+    dispatch(authedActions.loggedOut());
+    auth.logout();
+  };
+  return (
+    auth.loggedIn
+      ? (
+        <Button variant="light" size="sm" onClick={handleLogout}>OUTPUT</Button>
+      ) : null
+  );
+};
 
 const Frame = ({ children }) => {
   const { t } = useTranslation();
@@ -7,7 +28,8 @@ const Frame = ({ children }) => {
     <div className="d-flex flex-column h-100 bg-light">
       <Navbar className="bg-success bg-gradient shadow">
         <Container>
-          <Navbar.Brand href="#">{t('appFrame.navbar.appName')}</Navbar.Brand>
+          <Navbar.Brand href={paths.main}>{t('appFrame.navbar.appName')}</Navbar.Brand>
+          <ExitButton />
         </Container>
       </Navbar>
       {children}
