7818256382041898b09d998902b7c46695820bd5
hexlet_art <hexlet_art@mail.ru>
2023-07-21 18:00:47 +0300

    added realisation to scroll correctly

--- frontend/src/components/pages/chat/messages/additions/MessagesDisplay.jsx
+++ frontend/src/components/pages/chat/messages/additions/MessagesDisplay.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const MessagesDisplay = ({ messages }) => (
-  <div className="overflow-auto px-5" id="messages-container">
-    {messages.map(({ id, userName, body }) => (
-      <div className="text-brake mb-2" key={id}>
-        <b>
-          {`${userName}: `}
-        </b>
-        <span>
-          {body}
-        </span>
-      </div>
-    ))}
-  </div>
-);
+const MessagesDisplay = ({ messages }) => {
+  const dummy = useRef();
+
+  useEffect(() => {
+    dummy.current.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }, [messages]);
+
+  return (
+    <div className="overflow-auto px-5" id="messages-container">
+      {messages.map(({ id, userName, body }) => (
+        <div className="text-brake mb-2" key={id}>
+          <b>
+            {`${userName}: `}
+          </b>
+          <span>
+            {body}
+          </span>
+        </div>
+      ))}
+      <div ref={dummy} />
+    </div>
+  );
+};
 
 export default MessagesDisplay;
 // { body: "new message", channelId: 7, id: 8, username: "admin" }
