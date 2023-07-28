928838b18d4039bc2bb0738b423e0acd38ed93f3
hexlet_art <hexlet_art@mail.ru>
2023-07-28 16:18:28 +0300

    providers relocated

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthContext from '../contexts/AuthContext';
 import paths from '../paths.js';
 import Frame from './Frame';
 import RegistrationPage from './pages/RegistrationPage';
@@ -9,37 +8,23 @@ import LoginPage from './pages/AuthorizationPage';
 import NotFoundPage from './pages/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
 
-const AuthProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('userId'));
-  const login = (token, userName) => {
-    localStorage.setItem('userId', token);
-    localStorage.setItem('userName', userName);
-    setLoggedIn(true);
-  };
-  const logout = () => {
-    localStorage.removeItem('userId');
-    localStorage.removeItem('userName');
-    setLoggedIn(false);
-  };
-  return (
-    <AuthContext.Provider value={{ login, logout, loggedIn }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+import AuthProvider from './providers/AuthProvider';
+import LeoProfanityProvider from './providers/LeoProfanityProvider.jsx';
 
 const App = () => (
   <AuthProvider>
-    <Frame>
-      <Router>
-        <Routes>
-          <Route path={paths.any} element={<NotFoundPage />} />
-          <Route path={paths.main} element={<PrivateRoute />} />
-          <Route path={paths.login} element={<LoginPage />} />
-          <Route path={paths.signingUp} element={<RegistrationPage />} />
-        </Routes>
-      </Router>
-    </Frame>
+    <LeoProfanityProvider>
+      <Frame>
+        <Router>
+          <Routes>
+            <Route path={paths.any} element={<NotFoundPage />} />
+            <Route path={paths.main} element={<PrivateRoute />} />
+            <Route path={paths.login} element={<LoginPage />} />
+            <Route path={paths.signingUp} element={<RegistrationPage />} />
+          </Routes>
+        </Router>
+      </Frame>
+    </LeoProfanityProvider>
   </AuthProvider>
 );
 
