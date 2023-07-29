85faf832610985c2e132a744134d2be02943aff1
hexlet_art <hexlet_art@mail.ru>
2023-07-29 13:05:17 +0300

    added RollbarProvider

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import RollbarProvider from './providers/RollbarProvider';
 import AuthProvider from './providers/AuthProvider';
 import LeoProfanityProvider from './providers/LeoProfanityProvider';
 import paths from '../paths.js';
@@ -12,20 +13,22 @@ import NotFoundPage from './pages/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
 
 const App = () => (
-  <AuthProvider>
-    <LeoProfanityProvider>
-      <Frame>
-        <Router>
-          <Routes>
-            <Route path={paths.any} element={<NotFoundPage />} />
-            <Route path={paths.main} element={<PrivateRoute />} />
-            <Route path={paths.login} element={<LoginPage />} />
-            <Route path={paths.signingUp} element={<RegistrationPage />} />
-          </Routes>
-        </Router>
-      </Frame>
-    </LeoProfanityProvider>
-  </AuthProvider>
+  <RollbarProvider>
+    <AuthProvider>
+      <LeoProfanityProvider>
+        <Frame>
+          <Router>
+            <Routes>
+              <Route path={paths.any} element={<NotFoundPage />} />
+              <Route path={paths.main} element={<PrivateRoute />} />
+              <Route path={paths.login} element={<LoginPage />} />
+              <Route path={paths.signingUp} element={<RegistrationPage />} />
+            </Routes>
+          </Router>
+        </Frame>
+      </LeoProfanityProvider>
+    </AuthProvider>
+  </RollbarProvider>
 );
 
 export default App;
