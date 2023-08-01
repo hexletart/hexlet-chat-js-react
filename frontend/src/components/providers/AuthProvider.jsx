cf613ddf26b65523a0cb64f61ec28d60f8c5e32b
hexlet_art <hexlet_art@mail.ru>
2023-08-01 17:12:33 +0300

    resuming after logging

--- frontend/src/components/providers/AuthProvider.jsx
+++ frontend/src/components/providers/AuthProvider.jsx
@@ -6,12 +6,6 @@ const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('userId'));
 
   const login = (token, userName) => {
-    console.log();
-    console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    console.log('TOKEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', token);
-    console.log('USERNAME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', userName);
-    console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    console.log();
     localStorage.setItem('userId', token);
     localStorage.setItem('userName', userName);
     setLoggedIn(true);
