74e0faa9db62c400e77461d13a010695a081f6b2
hexlet_art <hexlet_art@mail.ru>
2023-08-02 02:40:14 +0300

    fixing test issue

--- frontend/src/components/pages/chat/messages/additions/MessagesInput.jsx
+++ frontend/src/components/pages/chat/messages/additions/MessagesInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Formik } from 'formik';
-import { Button, Form, InputGroup } from 'react-bootstrap';
+import { Col, Button, Form, InputGroup } from 'react-bootstrap';
 
 import { actions as messagesActions } from '../../../../../slices/messagesSlice';
 import { getAuthUserName } from '../../../../../tools/auth';
@@ -30,48 +30,52 @@ const MessagesInput = ({ channelId }) => {
         // const isDisabled = (values.message.length === 0) || isBlocking;
         return (
           <Form onSubmit={handleSubmit} className="py-1 border rounded-2">
-            <InputGroup>
-              <Form.Label aria-label={t('chatPage.authedChat.messages.form.input.ariaLabel')} htmlFor="formInputMessage" visuallyHidden>
-                Message
+
+            <Col className="my-1">
+              <Form.Label htmlFor="formMessagesInput" visuallyHidden>
+                {t('chatPage.authedChat.messages.form.input.ariaLabel')}
               </Form.Label>
-              <Form.Control
-                className="border-0 p-0 ps-2"
-                type="text"
-                name="message"
-                autoComplete="message"
-                placeholder={t('chatPage.authedChat.messages.form.input.placeholder')}
-                ref={(inputEl) => {
-                  inputEl?.focus();
-                }}
-                value={values.message}
-                onChange={handleChange}
-                // disabled={isBlocking}
-                aria-describedby="message-input"
-                id="formInputMessage"
-              />
-              <Button
-                type="submit"
-                variant=""
-                id="message-input-button"
-                className="btn-group-vertical border-0"
-              // disabled={isDisabled}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  // className={`${isDisabled ? 'text-muted' : 'text-success'} bi bi-envelope`}
-                  className="text-success"
-                  viewBox="0 0 16 16"
+              <InputGroup>
+                <Form.Control
+                  className="border-0 p-0 ps-2"
+                  type="text"
+                  name="message"
+                  autoComplete="message"
+                  placeholder={t('chatPage.authedChat.messages.form.input.placeholder')}
+                  ref={(inputEl) => {
+                    inputEl?.focus();
+                  }}
+                  value={values.message}
+                  onChange={handleChange}
+                  // disabled={isBlocking}
+                  aria-label={t('chatPage.authedChat.messages.form.input.ariaLabel')}
+                  id="formMessagesInput"
+                  aria-describedby="message-input"
+                />
+                <Button
+                  type="submit"
+                  variant=""
+                  id="message-input-button"
+                  className="btn-group-vertical border-0"
+                // disabled={isDisabled}
                 >
-                  <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                </svg>
-                <span className="visually-hidden">
-                  {t('chatPage.authedChat.messages.form.button.screenReader')}
-                </span>
-              </Button>
-            </InputGroup>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    // className={`${isDisabled ? 'text-muted' : 'text-success'} bi bi-envelope`}
+                    className="text-success"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                  </svg>
+                  <span className="visually-hidden">
+                    {t('chatPage.authedChat.messages.form.button.screenReader')}
+                  </span>
+                </Button>
+              </InputGroup>
+            </Col>
           </Form>
         );
       }}
