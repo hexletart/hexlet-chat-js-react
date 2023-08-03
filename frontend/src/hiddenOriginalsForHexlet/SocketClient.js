79683edc0eb53c5fab8cea64d562d51429f347ac
hexlet_art <hexlet_art@mail.ru>
2023-08-03 16:50:24 +0300

    archived hexlet related data

---
+++ frontend/src/hiddenOriginalsForHexlet/SocketClient.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+import { io } from 'socket.io-client';
+
+export default class SocketClient {
+  constructor() {
+    this.socket = io();
+    this.connected = false;
+  }
+
+  setConnected() {
+    this.connected = true;
+  }
+
+  setDisconnected() {
+    this.connected = false;
+  }
+
+  offAll() {
+    this.socket.removeAllListeners();
+  }
+
+  connect() {
+    if (!this.connected) {
+      this.socket.connect();
+    }
+  }
+
+  disconnect() {
+    if (this.connected) {
+      this.socket.disconnect();
+    }
+  }
+
+  emit(eventName, data) {
+    if (this.socket) {
+      this.socket.emit(eventName, data);
+    }
+  }
+
+  on(eventName, func) {
+    if (this.socket) {
+      this.socket.on(eventName, func);
+    }
+  }
+}
