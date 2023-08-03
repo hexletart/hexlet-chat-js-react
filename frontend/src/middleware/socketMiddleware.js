4fc363e67eb30834ca60e65a245b8b395ca95e68
hexlet_art <hexlet_art@mail.ru>
2023-08-03 10:42:01 +0300

    eslint-disable

--- frontend/src/middleware/socketMiddleware.js
+++ frontend/src/middleware/socketMiddleware.js
@@ -1,4 +1,4 @@
-// eslint-disable no-unused-expressions
+/* eslint-disable */
 import { actions as channelsActions } from '../slices/channelsSlice';
 import { actions as messagesActions } from '../slices/messagesSlice';
 
