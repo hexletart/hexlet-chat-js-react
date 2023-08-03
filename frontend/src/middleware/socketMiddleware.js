6b5db99da046bb2562efc7330b89724329b3f798
hexlet_art <hexlet_art@mail.ru>
2023-08-03 09:08:35 +0300

    changed elsint rule

--- frontend/src/middleware/socketMiddleware.js
+++ frontend/src/middleware/socketMiddleware.js
@@ -1,4 +1,4 @@
-/* eslint-disable functional/no-this-expressions */
+/* eslint-disable no-unused-expressions */
 import { actions as channelsActions } from '../slices/channelsSlice';
 import { actions as messagesActions } from '../slices/messagesSlice';
 
