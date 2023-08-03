f7c4948f457988e44e9b4c0ea67e86441b59518c
hexlet_art <hexlet_art@mail.ru>
2023-08-03 08:45:57 +0300

    getting back blockers after testing

--- frontend/src/components/pages/chat/messages/additions/MessagesInput.jsx
+++ frontend/src/components/pages/chat/messages/additions/MessagesInput.jsx
@@ -7,8 +7,7 @@ import { Button, Form, InputGroup } from 'react-bootstrap';
 import { actions as messagesActions } from '../../../../../slices/messagesSlice';
 import { getAuthUserName } from '../../../../../tools/auth';
 
-// const MessagesInput = ({ channelId, isBlocking }) => {
-const MessagesInput = ({ channelId }) => {
+const MessagesInput = ({ channelId, isBlocking }) => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
@@ -26,8 +25,7 @@ const MessagesInput = ({ channelId }) => {
       }}
     >
       {({ handleSubmit, handleChange, values }) => {
-        console.log();
-        // const isDisabled = (values.message.length === 0) || isBlocking;
+        const isDisabled = (values.message.length === 0) || isBlocking;
         return (
           <Form onSubmit={handleSubmit} className="py-1 border rounded-2">
             <InputGroup>
@@ -46,22 +44,21 @@ const MessagesInput = ({ channelId }) => {
                 }}
                 value={values.message}
                 onChange={handleChange}
-                // disabled={isBlocking}
+                disabled={isBlocking}
               />
               <Button
                 type="submit"
                 variant=""
                 id="message-input-button"
                 className="btn-group-vertical border-0"
-              // disabled={isDisabled}
+                disabled={isDisabled}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
-                  // className={`${isDisabled ? 'text-muted' : 'text-success'} bi bi-envelope`}
-                  className="text-success"
+                  className={`${isDisabled ? 'text-muted' : 'text-success'} bi bi-envelope`}
                   viewBox="0 0 16 16"
                 >
                   <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
